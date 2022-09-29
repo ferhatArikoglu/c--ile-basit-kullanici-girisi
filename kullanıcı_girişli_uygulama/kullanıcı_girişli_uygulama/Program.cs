0e4ef8b2175518246b0cc418f1b34ff93631d387
Ferhat Arıkoğlu <34ferhatarikoglu34@gmail.com>
2022-09-29 19:24:55 +0300

    c# if else ile kullanıcı girişi denetleme

---
+++ kullanıcı_girişli_uygulama/kullanıcı_girişli_uygulama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace kullanıcı_girişli_uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sifre;
+            string kullanici;
+            Console.WriteLine("kullanıcı adını giriniz");
+            kullanici = Console.ReadLine();
+            Console.WriteLine("şifrenizi giriniz");
+            sifre = Convert.ToInt32(Console.ReadLine());
+            if (sifre == 123 && kullanici == "ferhat")
+            {
+                Console.WriteLine("bilet alma uygulamasına hoşgeldiniz");
+            }
+            else
+            {
+                Console.WriteLine("kullanıcı adı veya şifreniz yanlış girilmiştir");
+            }
+        }
+    }
+}
